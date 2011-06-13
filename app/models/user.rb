a037e012db9041c4e61a26455c32f51c2450db10
Peter van der Klugt <vdklugt@hotmail.com>
2011-06-13 21:12:47 +0200

    Added password and encrypted_password attributes to the User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,17 +1,19 @@
 # == Schema Information
-# Schema version: 20110609194610
+# Schema version: 20110613190455
 #
 # Table name: users
 #
-#  id         :integer         not null, primary key
-#  name       :string(255)
-#  email      :string(255)
-#  created_at :datetime
-#  updated_at :datetime
+#  id                 :integer         not null, primary key
+#  name               :string(255)
+#  email              :string(255)
+#  created_at         :datetime
+#  updated_at         :datetime
+#  encrypted_password :string(255)
 #
 
 class User < ActiveRecord::Base
-  attr_accessible :name, :email
+  attr_accessor   :password
+  attr_accessible :name, :email, :password, :password_confirmation
   
   email_regex = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
 
@@ -24,4 +26,31 @@ class User < ActiveRecord::Base
   					:uniqueness => { :case_sensitive => false }
   # validates_format_of :email, :with => email_regex
 
+  validates :password, :presence => true,
+                       :confirmation => true,
+                       :length => { :within => 6..40 }
+
+#   before_save :encrypt_password
+#   
+#   def has_password?(submitted_password)
+#     encrypted_password == encrypt(submitted_password)
+#   end
+#   private
+#   
+#     def encrypt_password
+#       self.salt = make_salt if new_record?
+#       self.encrypted_password = encrypt(password)
+#     end
+#   
+#     def encrypt(string)
+#       secure_hash("#{salt}--#{string}")
+#     end
+#     
+#     def make_salt
+#       secure_hash("#{Time.now.utc}--#{password}")
+#     end
+#     
+#     def secure_hash(string)
+#       Digest::SHA2.hexdigest(string)
+#     end
 end
