f50e61bafd4fe46822feabc0db9d4ff0eb2946b3
Peter van der Klugt <vdklugt@hotmail.com>
2011-07-11 23:11:48 +0200

    Working user signin

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -12,8 +12,8 @@ class SessionsController < ApplicationController
       @title = "Sign in"
       render 'new'
     else
-#       sign_in user
-#       redirect_back_or user
+      sign_in user
+      redirect_to user
     end
   end
   
--- spec/controllers/sessions_controller_spec.rb
+++ spec/controllers/sessions_controller_spec.rb
@@ -38,26 +38,26 @@ describe SessionsController do
         flash.now[:error].should =~ /invalid/i
       end
     end
-#     
-#     describe "success" do
-#       
-#       before(:each) do
-#         @user = Factory(:user)
-#         @attr = { :email => @user.email, :password => @user.password }
-#       end
-# 
-#       it "should sign the user in" do
-#         post :create, :session => @attr
-#         controller.current_user.should == @user
-#         controller.should be_signed_in
-#       end
-# 
-#       it "should redirect to the user show page" do
-#         post :create, :session => @attr
-#         response.should redirect_to(user_path(@user))
-#       end
-#     end
-#   end
+    
+    describe "success" do
+      
+      before(:each) do
+        @user = Factory(:user)
+        @attr = { :email => @user.email, :password => @user.password }
+      end
+
+      it "should sign the user in" do
+        post :create, :session => @attr
+        controller.current_user.should == @user
+        controller.should be_signed_in
+      end
+
+      it "should redirect to the user show page" do
+        post :create, :session => @attr
+        response.should redirect_to(user_path(@user))
+      end
+    end
+  end
 #   
 #   describe "DELETE 'destroy'" do
 #     it "should sign a user out" do
@@ -66,5 +66,5 @@ describe SessionsController do
 #       controller.should_not be_signed_in
 #       response.should redirect_to(root_path)
 #     end
-  end
+#   end
 end
